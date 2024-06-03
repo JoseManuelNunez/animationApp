56fe29e6f74bd43be48773d8a866284ce80f2d06
JoseManuelNunez <josemanuelnunezreyes@gmail.com>
2024-06-03 17:34:20 -0400

    setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { RenderAnimation } from './components'
+
+function App() {
+
+  return (
+    <>
+    <RenderAnimation />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/animation/RenderAnimation.tsx
@@ -0,0 +1,10 @@
+import style from './animation.module.css'
+
+export const RenderAnimation = () => {
+  
+  return (
+    <div className={style.sprite}>
+      
+    </div>
+  )
+}
